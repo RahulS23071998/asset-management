eece56d38c7f4b0b038ce2cd167c01147b6102f9
RahulS23071998 <128679268+RahulS23071998@users.noreply.github.com>
2024-04-15 16:08:25 +0530

    Update AssetStatus.java

--- Desklite-Java/src/main/java/com/assetmanagement/desklite/asset/enums/AssetStatus.java
+++ Desklite-Java/src/main/java/com/assetmanagement/desklite/asset/enums/AssetStatus.java
@@ -3,5 +3,10 @@ package com.assetmanagement.desklite.asset.enums;
 public enum AssetStatus {
     ACTIVE,
     INACTIVE,
-
+    LOST,
+    BROKEN,
+    SOLD,
+    DONATED,
+    DISPOSED,
+    REPAIR
 }
